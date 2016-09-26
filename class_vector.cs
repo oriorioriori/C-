dc13e6ff43f993a3b98611f8f9a4ddd346d605e7
unknown <admin@VLAN-25-IKK-226.rd.mirea.ru>
2016-09-26 12:09:25 +0300

    vector

---
+++ class_vector.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class vector
+{
+    public int x, y, z;
+    public vector (int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public void printvector()
+    {
+        Console.WriteLine("x={0} y={1} z={2}", x, y, z);
+    }
+}
+
+    class Program
+{ 
+    static void Main()
+        {
+        vector vector1 = new vector(1, 2, 3);
+        vector1.printvector();
+    }
+   
+    }
+
